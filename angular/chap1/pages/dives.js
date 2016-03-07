37792f15924a5c7393d49972cbd26f4951c735de
Bernard Espanola <espanola.bernard@gmail.com>
2016-03-06 22:23:22 -0800

    began angular book. began chap 1. created static html page, jquery page and angular page.

---
+++ angular/chap1/pages/dives.js
@@ -0,0 +1,20 @@
+var dives = [
+    {
+        site: 'Abu Gotta Ramada',
+        location: 'Abu, Egypt',
+        depth: 72,
+        time: 54
+    },
+    {
+        site: 'Happy Gilmore',
+        location: 'Buenes, Ares',
+        depth: 54,
+        time: 38
+    },
+    {
+        site: 'Mt Rushmore',
+        location: 'Mexico, City',
+        depth: 98,
+        time: 62
+    }
+];
