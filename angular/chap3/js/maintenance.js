b74456b5295b468bdc1e2fc5978454d0ab53b80e
Bernard Espanola <espanola.bernard@gmail.com>
2016-03-14 12:00:13 -0700

    working on chap3. added a factory and directives. utilized angular DI methods

--- angular/chap3/js/maintenance.js
+++ angular/chap3/js/maintenance.js
@@ -1,16 +1,87 @@
 angular.module('maintenance', ['ngRoute'])
     .controller('adminCtrl', AdminCtrl)
+  angular.module('maintenance', ['ngRoute'])
+    .controller('adminCtrl', AdminCtrl)
+    .controller('mainCtrl', MainCtrl)
+    .controller('locationsCtrl', LocationsCtrl)
+    .controller('sitesCtrl', SitesCtrl)
+    .factory('currentSpot', currentSpot)
+    .directive('ywActiveMenu', ywActiveMenu)
+    .directive('ywMenuId', ywMenuId)
     .config(function ($routeProvider) {
         $routeProvider.when('/locations', {
-            templateUrl: './views/locations.html'
+            templateUrl: './views/locations.html',
+            controller: 'locationsCtrl'
         });
         $routeProvider.when('/sites', {
-            templateUrl: './views/sites.html'
+            templateUrl: './views/sites.html',
+            controller: 'sitesCtrl'            
         });
         $routeProvider.otherwise({
-            templateUrl: './views/main.html'
+            templateUrl: './views/main.html',
+            controller: 'mainCtrl'
         });
     });
 
-function AdminCtrl($scope) {
+function AdminCtrl($scope, currentSpot) {
+    $scope.isActive = isActive;
+    $scope.getTitle = getTitle;
+    
+    function isActive(menuId) {
+        return currentSpot.getActiveMenu() == menuId;
+    }
+    
+    function getTitle() {
+        return currentSpot.getTitle();
+    }
+}
+
+function MainCtrl () {
+}
+
+function LocationsCtrl() {
+}
+
+function SitesCtrl () {
+}
+
+function currentSpot() {
+    var activeMenuId = '';
+    var titleText = '';
+    
+    return {
+        setCurrentSpot: function(menuId, title) {
+            activeMenuId = menuId;
+            titleText = title;
+        },
+        getActiveMenu: function() {
+            return activeMenuId;
+        },
+        getTitle: function() {
+            return titleText;
+        }
+    }
+}
+
+function ywActiveMenu(currentSpot) {
+    return function(scope, element, attrs) {
+        var activeMenuId = attrs['ywActiveMenu'];
+        var activeTitle = attrs['ywActiveTitle'];
+        currentSpot.setCurrentSpot(activeMenuId, activeTitle);
+    }
+}
+
+function ywMenuId(currentSpot) {
+    function setActive(element, menuId) {
+        if (currentSpot.getActiveMenu() == menuId) {
+            element.addClass('active');
+        } else {
+            element.removeClass('active');
+        }
+    }
+    
+    return function (scropt, element, attrs) {
+        var menuId = attrs['ywMenuId']
+        setActive(element, menuId);
+    }
 }
